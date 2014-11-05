a1315fd6b1eef69a174f517143246fe1ac94484b
Varvara Efremova <varvara@echus.co>
2014-11-05 19:00:09 +1100

    Removed archive

--- _archive/drawing.py
+++
@@ -1,9 +0,0 @@
-#-----------------------------------------------------------
-# File drawing.py
-#-----------------------------------------------------------
-# Blender draw function wrappers
-
-import bpy
-import numpy as np
-import os
-
