4607b7e051f7ffb51ee84e9c00c34fd5d4ef64d6
Varvara Efremova <varvara@echus.co>
2014-11-20 19:58:12 +1100

    Testing new rngload

--- aptread/test-rngload.py
+++ aptread/test-rngload.py
@@ -1,19 +1,16 @@
-#!/usr/bin/python
 # test-ORNLRNGloader.py
-# ReferenceImplementationPython
-#
-# Author: Anna Ceguerra
-# Date: 5 March 2013
-# Copyright (c) 2013 Australian Centre for Microscopy & Microanalysis (ACMM), The University of Sydney, NSW 2006 Australia. All rights reserved.
 
-from ORNLRNGloader import ReadRng
+import os.path
 
-fn = raw_input('Enter ORNL rng filename: ')
-print fn
+from rngload import ReadRng
 
-rngfile = ReadRng(fn)
-print "ranges:", rngfile.rng
-print "range atoms:", rngfile.rngatoms
-print "n ranges:", rngfile.numrngs
-print "ions:", rngfile.ions
-print "n ions:", rngfile.numions
+PATH = "../data/test.rng"
+
+path = os.path.abspath(PATH)
+
+rngfile = ReadRng(path)
+#print "ranges:", rngfile.rng
+#print "range atoms/rngcomp:", rngfile.rngcomp
+#print "ions/atominfo:", rngfile.atominfo
+print("n ions/numatoms:", rngfile.natoms)
+print("n ranges:", rngfile.nrngs)
