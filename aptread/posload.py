b8608a4c5ec8ebb7ccbe1f26938aa7aa96aee7b4
Varvara Efremova <varvara@echus.co>
2014-12-05 11:19:38 +1100

    Added abstract posloader class

--- aptread/posload.py
+++ aptread/posload.py
@@ -8,16 +8,33 @@
 # Author: Varvara Efremova
 #
 # Description:
-# POS file loader
+# Posfile loader definition
 # =============================================================================
 
 import numpy as np
 
 class ReadError(Exception): pass
 
-class ReadPos():
-    def __init__(self, pos_fn):
-        self.numpts, self.xyz, self.mc = self.loadfile(pos_fn)
+class PosLoader():
+    """
+    Abstract XYZ pos loader definition:
+    POS loaders must implement these functions
+    """
+
+    def __init__(self, path):
+        self.npoints = None
+        self.xyz     = None
+        self.mc      = None
+
+        self.loadfile(path)
+
+    def loadfile(self, path):
+        return
+
+
+
+class POS(PosLoader):
+    """.pos loader"""
 
     # TODO more informative errors
     # TODO check it's actually a pos file
@@ -35,4 +52,8 @@ class ReadPos():
         numpoints = len(pos)
         xyz = pos[:,0:3]
         mc = pos[:,3]
-        return numpoints, xyz, mc
+
+        self.npoints = numpoints
+        self.xyz     = xyz
+        self.mc      = mc
+        return
