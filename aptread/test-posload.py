92c94a8027415d2fb0cd514dd9c179c33d54bab8
Varvara Efremova <varvara@echus.co>
2014-12-05 11:19:55 +1100

    Updated test for new posload.py

--- aptread/test-posload.py
+++ aptread/test-posload.py
@@ -6,11 +6,11 @@
 # Date: 5 March 2013
 # Copyright (c) 2013 Australian Centre for Microscopy & Microanalysis (ACMM), The University of Sydney, NSW 2006 Australia. All rights reserved.
 
-from POSloader import ReadPos
+from posload import POS
 
-pos_fn = raw_input('Enter pos filename: ')
+path = "/home/user/Blender/2.72/scripts/addons/atomblend/data/R04.pos"
 
-posfile = ReadPos(pos_fn)
-print posfile.numpoints
-print posfile.xyz[10:15]
-print posfile.cm[10:15]
+posfile = POS(path)
+print("Number of points:", posfile.npoints)
+print("XYZ", posfile.xyz[10:15])
+print("mc", posfile.mc[10:15])
